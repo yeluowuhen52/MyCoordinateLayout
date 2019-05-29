953b7714ecae7e160f741a1c010db2670f369f32
jtq(江天麒) <JTQc5235>
2019-05-29 09:17:03 +0800

    Initial commit

---
+++ app/src/main/java/com/marsjiang/mycoordinatelayout/ScrollingActivity.java
@@ -0,0 +1,82 @@
+package com.marsjiang.mycoordinatelayout;
+
+import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class ScrollingActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private SwipeRefreshLayout swipeRefreshLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling);
+
+        recyclerView = (RecyclerView) findViewById(R.id.rv);
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
+
+//        recyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
+//            @Override
+//            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+//                int topRowVerticalPosition =
+//                        (recyclerView == null || recyclerView.getChildCount() == 0) ? 0 : recyclerView.getChildAt(0).getTop();
+//                Log.d("testtest", topRowVerticalPosition + "");
+////                if (topRowVerticalPosition >= 0) {
+////                    swipeRefreshLayout.setEnabled(true);
+////                }else if(swipeRefreshLayout.isRefreshing()){
+////                    swipeRefreshLayout.setRefreshing(false);
+////                }
+//
+////                if (swipeRefreshLayout.isRefreshing()) {
+////                    swipeRefreshLayout.setRefreshing(false);
+////                } else {
+//                swipeRefreshLayout.setEnabled(topRowVerticalPosition > 0);
+////                }
+//            }
+//
+//            @Override
+//            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+//                super.onScrollStateChanged(recyclerView, newState);
+//            }
+//        });
+
+        recyclerView.setAdapter(new RecyclerView.Adapter() {
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                return new ViewHolder(getLayoutInflater().inflate(R.layout.item_simple, parent, false));
+            }
+
+            @Override
+            public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+                ViewHolder vh = (ViewHolder) holder;
+                vh.text.setText("Fake Item " + (position + 1));
+                vh.text2.setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
+            }
+
+            @Override
+            public int getItemCount() {
+                return 20;
+            }
+
+            class ViewHolder extends RecyclerView.ViewHolder {
+
+                TextView text;
+                TextView text2;
+
+                public ViewHolder(View itemView) {
+                    super(itemView);
+
+                    text = (TextView) itemView.findViewById(R.id.text);
+                    text2 = (TextView) itemView.findViewById(R.id.text2);
+                }
+
+            }
+        });
+    }
+}
